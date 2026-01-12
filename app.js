e4c1532b46dfb6dd2a76df8b6a1d1b22054f146e
tahin18t <tahin18t@gmail.com>
2026-01-12 23:37:55 +0600

    got error in deploy, use express 5 safe fallback

--- app.js
+++ app.js
@@ -78,10 +78,11 @@ app.use('/api/v1', router)
 app.use(express.static(path.join(__dirname, "client/dist")));
 
 // Add React front End Routing
-app.use(express.static(path.join(__dirname, "client", "dist")));
+app.use(express.static(path.join(__dirname, "client/dist")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+/* ⚠️ EXPRESS 5 SAFE FALLBACK */
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "client/dist/index.html"));
 });
 
 
